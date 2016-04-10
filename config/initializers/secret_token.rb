591001c99dd8374221e00805811bbff3fb8bc4e8
ck <thecurrencykid@gmail.com>
2016-04-10 16:09:48 -0700

    railas 3.2.22.2

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Treeclone::Application.config.secret_token = 'bf22b6c9f619bddf75eb7f55d237fdcae718c8347010f643a676d36b67b17e6495bae577056c3be01939387c7dea53bfcf6186082653eb17d2c2f32808538bbf'
